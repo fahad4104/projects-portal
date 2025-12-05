7f8e94499c6919effb0a22857c5b023529a49928
fahad4104 <uaeguy28@gmail.com>
2025-12-05 05:28:16 +0400

    Init Prisma migrations for Neon

--- prisma/migrations/20251124010031_add_project_photos/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "ProjectPhoto" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "projectId" TEXT NOT NULL,
-    "fileName" TEXT NOT NULL,
-    "filePath" TEXT NOT NULL,
-    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "ProjectPhoto_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
--- prisma/migrations/20251203102514_add_project_messages/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "ProjectMessage" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "projectId" TEXT NOT NULL,
-    "authorRole" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "ProjectMessage_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
